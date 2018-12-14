c2c7878c680331d5185f252e476787980b17eada
Keith Stedman <keithstedman@gmail.com>
2018-12-14 13:22:15 -0800

    local build

---
+++ build/precache-manifest.0a51e85fe6a16e07ba13e1218d003c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "bf398c1bdf9cde9cc01e",
+    "url": "/react-app/static/js/main.bf398c1b.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-app/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "bf398c1bdf9cde9cc01e",
+    "url": "/react-app/static/css/main.1bc2d7a6.chunk.css"
+  },
+  {
+    "revision": "31f0c1eebc08ac54c2d9e682cc9e7fde",
+    "url": "/react-app/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.942c492066acf1678a93118333084d0e.js"
+  "/react-app/precache-manifest.0a51e85fe6a16e07ba13e1218d003c39.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
