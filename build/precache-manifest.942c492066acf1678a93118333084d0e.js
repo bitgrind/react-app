2a0fb0f4bdddaa4d2a18fcae6f171e065beb9848
Keith Stedman <keithstedman@gmail.com>
2018-12-14 13:14:48 -0800

    deploy

---
+++ build/precache-manifest.942c492066acf1678a93118333084d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf20deabcb90a1013919",
+    "url": "/static/js/main.cf20deab.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "cf20deabcb90a1013919",
+    "url": "/static/css/main.1bc2d7a6.chunk.css"
+  },
+  {
+    "revision": "461f894da2826dd5d7d08a371a014861",
+    "url": "/index.html"
+  }
+];
